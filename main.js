967352d1234a144684b6b27fd22d0830e38e5b4a
Saanvi-2208 <144683656+Saanvi-2208@users.noreply.github.com>
2023-09-11 16:44:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function change()
+{
+  document.getElementById("image1").src="https://gifdb.com/images/high/sparkling-tulip-flowers-ke58y8gzibupnzx2.gif"; 
+}
+
+function change1()
+{
+  document.getElementById("image2").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrs-HEx5k2mJks13d7Cla3Ms9DKJjD9XfDu_BmHTHqxV5gZBYffXf7LAtIy2P0hadELNE&usqp=CAU"; 
+}
+
+function change2()
+{
+  document.getElementById("image3").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiHk64ijjrkRtWpCMn2XgejfQq48Oi-MowWQ&usqp=CAU"; 
+}
+
+function change3()
+{
+  document.getElementById("image4").src="https://i.pinimg.com/originals/8b/1e/79/8b1e799f3b40dd088e06439333e221c4.gif";
+}
